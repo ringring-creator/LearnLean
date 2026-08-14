618ef4203181bda0e0e7c36aa2ab4005f8ed26b9
RingRing <ringring.creator@gmail.com>
2026-08-14 09:26:47 +0900

    add NotationSimp.lean

--- LearnLean/NatOrder/NotationSimp.lean
+++ LearnLean/NatOrder/NotationSimp.lean
@@ -23,6 +23,7 @@ macro_rules
 | `(tactic| notation_simp $[[$simpArgs,*]]? $[at $location]?) =>
   let args := simpArgs.map (·.getElems) |>.getD #[]
   `(tactic| simp only [notation_simp, $args,*] $[at $location]?)
+
 end
 
 attribute [notation_simp] MyNat.lt_def
@@ -40,3 +41,34 @@ example (m n : MyNat) (h : m < n) : m + 1 ≤ n := by
 example (m n : MyNat) : m < n := by
   -- `simp` made no progress
   -- simp
+
+section
+open Lean Parser Tactic
+
+syntax "notation_simp?" (simpArgs)? (location)? : tactic
+
+macro_rules
+| `(tactic| notation_simp? $[[$simpArgs,*]]? $[at $location]?) =>
+  let args := simpArgs.map (·.getElems) |>.getD #[]
+  `(tactic| simp? only [notation_simp, $args,*] $[at $location]?)
+
+end
+
+example (m n : MyNat) : m < n := by
+  notation_simp?
+  -- Try this:
+  -- [apply] simp only [MyNat.lt_def]
+
+example (a b : MyNat) (h1 : a < b) (h2 : b < a) : False := by
+  notation_simp at *
+  have :  b + 1 ≤ b := calc
+    _ ≤ a := by apply h2
+    _ ≤ a + 1 := by exact MyNat.le_add_one_right a
+    _ ≤ b := by apply h1
+
+  rw [MyNat.le_iff_add] at this -- this : ∃ k, b + 1 + k = b
+  obtain ⟨k, hk⟩ := this
+  have : 1 + k = 0 := by
+    rw [show b + 1 + k = b + (1 + k) from by ac_rfl] at hk
+    simp_all
+  simp_all
